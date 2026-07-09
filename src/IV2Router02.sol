bd9afc699f801313d09065c5068a5eeb52ea43f9
k2gutierrez <cchimal.gutierrez@gmail.com>
2026-07-09 17:45:50 -0600

    comments in functions

--- src/IV2Router02.sol
+++ src/IV2Router02.sol
@@ -3,6 +3,15 @@ pragma solidity 0.8.30;
 
 interface IV2Router02 {
 
+    /**
+     * @dev Uniswap function to swap exact tokens for other tokens
+     * @param amountIn Input Amount
+     * @param amountOutMin Minimum amount Out
+     * @param path Array of token address for the swap
+     * @param to Address to send the tokens
+     * @param deadline Time allowed to expect for the swap
+     * @return amounts Array of amounts
+     */
     function swapExactTokensForTokens(
         uint amountIn,
         uint amountOutMin,
--- src/SwapApp.sol
+++ src/SwapApp.sol
@@ -7,15 +7,20 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 
 contract SwapApp {
 
+    // Calling SafeERC20 library to use in the IERC20 interface
     using SafeERC20 for IERC20;
 
+    // Address of the V2 Router 02
     address public s_V2Router02Address;
+    // Address of the fee receiver
     address public s_FeeReceiver;
 
+    // Amount of fee received
     uint256 private s_totalFeeReceived;
     uint256 private constant PERCENTAGE_BASIS = 1000;
     uint256 private s_feeBasisPoints = 25; // basis of 1000 e.g., 25 = 2.5% (amount * s_feeBasisPoints / PERCENTAGE_BASIS)
 
+    // Events
     event SwapTokens(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut);
 
     constructor(address _V2Router02Address, address _feeReceiver) {
@@ -23,6 +28,13 @@ contract SwapApp {
         s_FeeReceiver = _feeReceiver;
     }
 
+    /**
+     * @dev Function to swap tokens
+     * @param _amountIn Input amount
+     * @param _amountOutMin Minimum amount out
+     * @param _path Array of token address to swap
+     * @param _deadline Time allowed to expect for the swap
+     */
     function swapTokens(uint256 _amountIn, uint256 _amountOutMin, address[] memory _path, uint256 _deadline) external {
         
         uint256 fee = (_amountIn * getFeeBasisPoints()) / getPercentageBasis();
@@ -41,14 +53,26 @@ contract SwapApp {
         emit SwapTokens(_path[0], _path[_path.length - 1], _amountIn, amountOut[amountOut.length - 1]);
     }
 
+    /**
+     * @dev Get the total fee received
+     * @return totalFeeReceived Total fee received
+     */
     function getTotalFeeReceived() external view returns(uint256 totalFeeReceived) {
         totalFeeReceived = s_totalFeeReceived;
     }
 
+    /**
+     * @dev get the fee Basis Points for the operation %
+     * @return feeBasisPoints Fee basis points
+     */
     function getFeeBasisPoints() public view returns(uint256 feeBasisPoints) {
         feeBasisPoints = s_feeBasisPoints;
     }
 
+    /**
+     * @dev Get the Percentage Basis
+     * @return percentageBasis Percentage Basis
+     */
     function getPercentageBasis() public pure returns(uint256 percentageBasis) {
         percentageBasis = PERCENTAGE_BASIS;
     }
